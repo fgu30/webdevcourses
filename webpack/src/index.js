51a88da29cf9b5dec967da41127e79db06c6f998
lurongtao <lurongtao@126.com>
2020-10-21 10:33:25 +0800

    2020-10-21-am

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,10 @@
-console.log(1)
+import str from './Header'
+
+import './style/style.css'
+import './style/body.styl'
+
+import img from './assets/images/QQ20201017-0.png'
+
+str()
+
+document.querySelector('body').innerHTML += `<img src="${img}" />`
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: 'development',
@@ -12,7 +13,7 @@ module.exports = {
   //   insurance: './src/index.js'
   // },
   entry: {
-    app: './src/main.js'
+    'scripts/app': './src/index.js'
   },
 
   // 出口
@@ -21,18 +22,69 @@ module.exports = {
     filename: '[name].js'
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: 'styles'
+            }
+          }, 
+          'css-loader'
+        ]
+      },
+
+      {
+        test: /\.styl$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader']
+      },
+
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 2000,
+            outputPath: 'images',
+            name() {
+              return '[contenthash:6].[ext]';
+            },
+          }
+        }
+      }
+    ]
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
       filename: 'index.html',
       inject: true
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: ({ chunk }) => {
+        return `${chunk.name.replace('scripts', 'styles')}.css`
+      }
     })
   ],
 
   // 配置server
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
-    port: 8080,
-    quiet: true
+    port: 8080
   }
 }
